30bbd269c02c02c730892b99d3a61044029fec08
Joshua Nilsson <27570195+joshua-nilsson@users.noreply.github.com>
2022-07-04 07:38:34 -0600

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,17 +1,13 @@
 import NextAuth from "next-auth";
-import Providers from "next-auth/providers";
-import { db } from "../../../firebase";
-import { FirebaseAdapter } from "@next-auth/firebase-adapter";
+import GoogleProvider from "next-auth/providers/google";
 
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
-    Providers.Google({
+    GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
     }),
     // ...add more providers here
   ],
-
-  adapter: FirebaseAdapter(db),
 });
