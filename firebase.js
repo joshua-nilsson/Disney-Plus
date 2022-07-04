eb592e90096c148e3dfd73680f93ed90f140cf59
Joshua Nilsson <27570195+joshua-nilsson@users.noreply.github.com>
2022-07-04 09:58:09 -0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,7 +1,4 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp, getApp, getApps } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+import firebase from "firebase";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_APP_API_KEY,
@@ -12,10 +9,10 @@ const firebaseConfig = {
   appId: process.env.NEXT_APP_ID
 };
 
-// Initialize Firebase
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-const db = getFirestore();
-const storage = getStorage();
+const app = !firebase.apps.length
+  ? firebase.initializeApp(firebaseConfig)
+  : firebase.app();
 
-export default app;
-export { db, storage };
+const db = app.firestore();
+
+export { db };
