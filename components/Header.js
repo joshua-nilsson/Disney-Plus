e3aa5049115055984c297c66281e81c7994f7faf
Joshua Nilsson <27570195+joshua-nilsson@users.noreply.github.com>
2023-06-01 02:45:14 -0600

    Added HeadlessUI + Modal Component

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Modal from "./Modal";
 import {
   HomeIcon,
   SearchIcon,
@@ -13,57 +14,62 @@ function Header() {
   const router = useRouter();
 
   return (
-    <header className="sticky bg-[#040714] top-0 z-[1000] flex items-center px-10 md:px-12 h-[72px]">
-      <Image
-        src="/images/logo.svg"
-        alt=""
-        width={80}
-        height={80}
-        className="cursor-pointer"
-        onClick={() => router.push("/")}
-      />
-      {session && (
-        <div className="hidden ml-10 md:flex items-center space-x-6">
-          <a className="header-link group">
-            <HomeIcon className="h-4" />
-            <span className="span">Home</span>
-          </a>
-          <a className="header-link group">
-            <SearchIcon className="h-4" />
-            <span className="span">Search</span>
-          </a>
-          <a className="header-link group">
-            <PlusIcon className="h-4" />
-            <span className="span">Watchlist</span>
-          </a>
-          <a className="header-link group">
-            <StarIcon className="h-4" />
-            <span className="span">Originals</span>
-          </a>
-          <a className="header-link group">
-            <img src="/images/movie-icon.svg" alt="" className="h-5" />
-            <span className="span">Movies</span>
-          </a>
-          <a className="header-link group">
-            <img src="/images/series-icon.svg" alt="" className="h-5" />
-            <span className="span">Series</span>
-          </a>
-        </div>
-      )}
-      {!session ? (
-        <button
-          className="ml-auto uppercase border px-4 py-1.5 rounded font-medium tracking-wide hover:bg-white hover:text-black transition duration-200"
-          onClick={signIn}
-        >
-          Login
-        </button>
-      ) : (
-        <img
-          src={session.user.image}
-          className="ml-auto h-12 w-12 rounded-full object-cover cursor-pointer"
-          onClick={signOut}
+    <header className="sticky bg-[#040714] top-0 z-[1000] flex items-center justify-between px-10 md:px-12 h-[72px]">
+      <div className="flex">
+        <Image
+          src="/images/logo.svg"
+          alt=""
+          width={80}
+          height={80}
+          className="cursor-pointer"
+          onClick={() => router.push("/")}
         />
-      )}
+        {session && (
+          <div className="hidden ml-10 md:flex items-center space-x-6">
+            <a className="header-link group">
+              <HomeIcon className="h-4" />
+              <span className="span">Home</span>
+            </a>
+            <a className="header-link group">
+              <SearchIcon className="h-4" />
+              <span className="span">Search</span>
+            </a>
+            <a className="header-link group">
+              <PlusIcon className="h-4" />
+              <span className="span">Watchlist</span>
+            </a>
+            <a className="header-link group">
+              <StarIcon className="h-4" />
+              <span className="span">Originals</span>
+            </a>
+            <a className="header-link group">
+              <img src="/images/movie-icon.svg" alt="" className="h-5" />
+              <span className="span">Movies</span>
+            </a>
+            <a className="header-link group">
+              <img src="/images/series-icon.svg" alt="" className="h-5" />
+              <span className="span">Series</span>
+            </a>
+          </div>
+        )}
+      </div>
+      <div className="flex items-center">
+        <Modal />
+        {!session ? (
+          <button
+            className="ml-auto uppercase border px-4 py-1.5 rounded font-medium tracking-wide hover:bg-white hover:text-black transition duration-200"
+            onClick={signIn}
+          >
+            Login
+          </button>
+        ) : (
+          <img
+            src={session.user.image}
+            className="ml-auto h-12 w-12 rounded-full object-cover cursor-pointer"
+            onClick={signOut}
+          />
+        )}
+      </div>
     </header>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
+const { screens } = require('tailwindcss/defaultTheme');
+
 module.exports = {
   mode: "jit",
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+		screens: {
+			xs: '350px',
+			msm: '425px',
+			...screens,
+			'3xl': '2250px',
+	  },
     extend: {
       backgroundImage: (theme) => ({
         home: "url('/images/background.png')",
